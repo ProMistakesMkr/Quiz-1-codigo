dea3c524819576419469081d7066d98fb1c383cb
Santiago Granobles <santiago.granobles@hotmail.com>
2021-04-11 21:45:27 -0500

    empezando controller

---
+++ src/controller/Controller.java
@@ -0,0 +1,18 @@
+package controller;
+
+import model.Logic;
+import processing.core.PApplet;
+
+public class Controller {
+
+	Logic logic;
+	PApplet app;
+	
+	public Controller(PApplet app) {
+		this.app = app;
+		logic = new Logic(app);
+	}
+	
+	public void drawCircle() {
+		logic.drawCircle();
+	}}
